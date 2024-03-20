03155b9ce46b35bb8bdc8835755cd39013617bf2
André Silva <andreluiz.97@outlook.com>
2024-03-20 19:26:46 -0300

    feat: improve home design

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,30 @@
 <template>
-  <div class="home">
-    <NuxtLink to="/csr">CSR</NuxtLink>
-    <NuxtLink to="/ssr">SSR</NuxtLink>
-    <NuxtLink to="/ssg">SSG</NuxtLink>
-    <NuxtLink to="/isr">ISR</NuxtLink>
-  </div>
+  <main class="home">
+    <h1>Rendering Modes Demo</h1>
+    <NuxtLink to="/csr">Client-Side Rendering (CSR)</NuxtLink>
+    <NuxtLink to="/ssr">Server-Side Rendering (SSR)</NuxtLink>
+    <NuxtLink to="/ssg">Static Site Generation (SSG)</NuxtLink>
+    <NuxtLink to="/isr">Incremental Static Regeneration (ISG)</NuxtLink>
+  </main>
 </template>
 
 <style scoped>
 .home {
+  width: 100%;
+  height: 100%;
   display: flex;
+  justify-content: center;
+  align-items: center;
   flex-direction: column;
   gap: 16px;
+  background-color: #2f2b42;
+}
+
+h1 {
+  color: #ffb633;
+}
+
+a {
+  color: #aaa3cc
 }
 </style>
